5f3bef2d1156b2d95fe8401b1cd1ac0d489bd2e2
01Nijat <mr.nijat.asgarov@gmail.com>
2022-12-02 00:29:00 +0400

    Yaradildi

---
+++ Cust0mL1st/Collection/MyList.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cust0mL1st.Collection
+{
+    public class MyList<T>
+    {
+        private T[] arr;
+        public int Count { get; set; }
+        public MyList()
+        {
+            arr = new T[10];
+        }
+        public void AddRange(IEnumerable<T> list)
+        {
+            foreach (var item in list)
+            {
+                add(item);
+            }
+        }
+        public void add(T obj)
+        {
+            if (Count%10==0)
+            {
+                ArrayResize();
+            }
+            arr[Count] = obj;
+            Count++;
+        }
+        private void ArrayResize()
+        {
+            Array.Resize(ref arr, arr.Length + 10);
+        }
+        public void Clear()
+        {
+            arr= new T[10];
+            Count= 0;
+        }
+        public T Find(Predicate<T> func)
+        {
+            foreach (var item in arr)
+            {
+                if (func(item))
+                {
+                    return item;
+                }
+            }
+            return default;
+        }
+        public bool Contains(T obj)
+        {
+            foreach (var item in arr)
+            {
+                if (Comparer<T>.Default.Compare(item, obj) == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Cust0mL1st/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Cust0mL1st.Collection;
+
+MyList<string> list = new MyList<string>();
+list.add("Azerbaijan");
+list.add("Turkish");
+list.add("Usa");
+list.add("Rusia");
+list.add("Ukraina");
+list.add("Poland");
+list.add("Germany");
+list.add("England");
+list.add("China");
+list.add("Japan");
+list.add("Korea");
+list.add("Pakistan");
+MyList<int>list2 = new MyList<int>();
+Console.WriteLine(list2.Find(i=>i==100));
+list2.add(1);
+list2.add(2);
+list2.add(3);
+Console.WriteLine(list.Contains("Azerbaijan"));
